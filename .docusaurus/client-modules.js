c1700f75b8cf3eec0e6b2a1c062a908c4d208d63
ywjr <171174433@qq.com>
2020-09-19 09:30:17 +0800

    commit init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("C:\\doc\\xdoc.github.io\\xdoc.github.io\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\doc\\xdoc.github.io\\xdoc.github.io\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\doc\\xdoc.github.io\\xdoc.github.io\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\doc\\xdoc.github.io\\xdoc.github.io\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\doc\\xdoc.github.io\\xdoc.github.io\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
+  require("C:\\doc\\xdoc.github.io\\xdoc.github.io\\src\\css\\custom.css"),
+];
---
+++ .docusaurus/docusaurus.config.js
@@ -0,0 +1,121 @@
+export default {
+  "title": "文档处理中心",
+  "tagline": "AI doc hub",
+  "url": "https://xdoc.github.io",
+  "baseUrl": "/",
+  "onBrokenLinks": "throw",
+  "favicon": "img/favicon.ico",
+  "organizationName": "ywjr",
+  "projectName": "xdoc.github.io",
+  "themeConfig": {
+    "navbar": {
+      "title": "智能文档中心",
+      "logo": {
+        "alt": "My Site Logo",
+        "src": "img/logo.svg"
+      },
+      "items": [
+        {
+          "to": "docs/",
+          "activeBasePath": "docs",
+          "label": "Docs",
+          "position": "left"
+        },
+        {
+          "to": "blog",
+          "label": "Blog",
+          "position": "left"
+        },
+        {
+          "href": "https://github.com/facebook/docusaurus",
+          "label": "GitHub",
+          "position": "right"
+        }
+      ],
+      "hideOnScroll": false
+    },
+    "footer": {
+      "style": "dark",
+      "links": [
+        {
+          "title": "Docs",
+          "items": [
+            {
+              "label": "Style Guide",
+              "to": "docs/"
+            },
+            {
+              "label": "Second Doc",
+              "to": "docs/doc2/"
+            }
+          ]
+        },
+        {
+          "title": "Community",
+          "items": [
+            {
+              "label": "Stack Overflow",
+              "href": "https://stackoverflow.com/questions/tagged/docusaurus"
+            },
+            {
+              "label": "Discord",
+              "href": "https://discordapp.com/invite/docusaurus"
+            },
+            {
+              "label": "Twitter",
+              "href": "https://twitter.com/docusaurus"
+            }
+          ]
+        },
+        {
+          "title": "More",
+          "items": [
+            {
+              "label": "Blog",
+              "to": "blog"
+            },
+            {
+              "label": "GitHub",
+              "href": "https://github.com/facebook/docusaurus"
+            }
+          ]
+        }
+      ],
+      "copyright": "Copyright © 2020 My Project, Inc. Built with Docusaurus."
+    },
+    "colorMode": {
+      "defaultMode": "light",
+      "disableSwitch": false,
+      "respectPrefersColorScheme": false,
+      "switchConfig": {
+        "darkIcon": "🌜",
+        "darkIconStyle": {},
+        "lightIcon": "🌞",
+        "lightIconStyle": {}
+      }
+    },
+    "metadatas": []
+  },
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "sidebarPath": "C:\\doc\\xdoc.github.io\\xdoc.github.io\\sidebars.js",
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/"
+        },
+        "blog": {
+          "showReadingTime": true,
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/blog/"
+        },
+        "theme": {
+          "customCss": "C:\\doc\\xdoc.github.io\\xdoc.github.io\\src\\css\\custom.css"
+        }
+      }
+    ]
+  ],
+  "onDuplicateRoutes": "warn",
+  "customFields": {},
+  "plugins": [],
+  "themes": []
+};
